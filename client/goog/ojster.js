4b6ca7e46cbbd28965be52cda12eefac915e5539
Smolin Dmitry <dimsmol@gmail.com>
2011-07-16 23:14:37 +0400

    only one way of template rendering left, all helper functions hackery is removed

--- client/goog/ojster.js
+++ client/goog/ojster.js
@@ -12,6 +12,7 @@ ojster.StringWriter = function() {
 	this.buff = [];
 };
 
+// used to get true writer if wrapper object is passed instead (see Template class code)
 ojster.StringWriter.prototype.getWriter = function() {
 	return this;
 };
@@ -32,7 +33,7 @@ ojster.DefaultWriterClass = ojster.StringWriter;
 ojster.Template = function(ctx, data, writer) {
 	this.ctx = ctx;
 	this.data = data;
-	this.writer = writer == null ? new DefaultWriterClass() : writer.getWriter();
+	this.writer = writer == null ? new DefaultWriterClass() : writer.getWriter(); // writer argument can actually be writer's wrapper, unwrapping it
 };
 
 // an example, will be overriden by child template
@@ -50,6 +51,7 @@ ojster.Template.prototype.escape = function(str) {
 	return ojster.escape(str);
 };
 
+// used to get true writer if template is passed instead of it
 ojster.Template.prototype.getWriter = function() {
 	return this.writer;
 };
@@ -61,22 +63,6 @@ ojster.Template.prototype.write = function() {
 
 ojster.Template.prototype.done = function() {
     return this.writer.done();
-};
-
-ojster.Template.prototype.template = function(TemplateClass, ctx, data) {
-	return new TemplateClass(ctx, data, this);
-};
-
-ojster.Template.prototype.renderTemplate = function(TemplateClass, ctx, data, args) {
-	var instance = this.template(templateClass, ctx, data);
-	return instance.render.apply(instance, args);
-};
-
-ojster.Template.prototype.getRendered = function() {
-	if (arguments.length == 0) {
-		return this.renderBlockMain().done();
-	}
-	return this.renderBlockMain.apply(this, arguments).done();
 };
 
 ojster.Template.prototype.render = function() {
@@ -96,18 +82,13 @@ ojster.escape = function(str) {
 			       .replace(/"/g, '&quot;');
 };
 
-ojster.template = function(templateClass, ctx, data, writer) {
-	var instance = new templateClass(ctx, data, writer);
-	return instance;
-};
-
-ojster.render = function(templateClass, ctx, data, args, writer) {
-	var instance = ojster.template(templateClass, ctx, data, writer);
-	return instance.render.apply(instance, args);
-};
-
-ojster.getRendered = function(templateClass, ctx, data, args, writer) {
+ojster._getRendered = function(templateClass, ctx, data, args, writer) {
 	return ojster.render(templateClass, ctx, data, args, writer).done();
+};
+
+ojster.fillElement = function(element, html) {
+	element.innerHTML = html;
+	return element;
 };
 
 ojster.createElement = function(html, domHelper) {
@@ -131,15 +112,14 @@ ojster.createFragment = function(html, domHelper) {
 };
 
 ojster.renderToElement = function(element, templateClass, ctx, data, args) {
-	element.innerHTML = ojster.getRendered(templateClass, ctx, data, args);
-	return element;
+	return ojster.fillElement(element, ojster._getRendered(templateClass, ctx, data, args));
 };
 
 ojster.renderAsElement = function(templateClass, ctx, data, args, domHelper) {
-	return ojster.createElement(ojster.getRendered(templateClass, ctx, data, args), domHelper);
+	return ojster.createElement(ojster._getRendered(templateClass, ctx, data, args), domHelper);
 };
 
 ojster.renderAsFragment = function(templateClass, ctx, data, args, domHelper) {
-	return ojster.createFragment(ojster.getRendered(templateClass, ctx, data, args), domHelper);
+	return ojster.createFragment(ojster._getRendered(templateClass, ctx, data, args), domHelper);
 };
 
